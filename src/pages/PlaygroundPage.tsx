8a6b26e976b13cceef60d3b34e826ee5ee5bf26a
UnicornDad8 <unicorndad8@gmail.com>
2024-12-18 14:28:07 -0300

    Test SearchBox component with onChange prop

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,10 +1,7 @@
-import ExpandableText from "../components/ExpandableText";
-
+import SearchBox from "../components/SearchBox";
 
 const PlaygroundPage = () => {
-  const text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-
-  return <ExpandableText text={text} />
+  return <SearchBox onChange={(text) => console.log(text)} />
 };
 
 export default PlaygroundPage;
