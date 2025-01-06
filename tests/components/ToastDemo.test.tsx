b750f0a8e9eb4ad8ed9af8d882f9f5c16e3a0e9d
UnicornDad8 <unicorndad8@gmail.com>
2025-01-06 11:36:02 -0300

    Finish test OrderStatusSelector component

--- tests/components/ToastDemo.test.tsx
+++ tests/components/ToastDemo.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import ToastDemo from '../../src/components/ToastDemo';
 import { Toaster } from 'react-hot-toast';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react';
 
 describe("ToastDemo", () => {
   it("should render a toast",async () => {
@@ -15,7 +14,7 @@ describe("ToastDemo", () => {
 
     const button = screen.getByRole('button');
     const user = userEvent.setup();
-    await act(() => user.click(button));
+    await user.click(button);
 
     const toast = await screen.findByText(/success/i);
     expect(toast).toBeInTheDocument();
