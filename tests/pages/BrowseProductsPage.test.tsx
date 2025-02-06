706a82381f5e90763316d3ee884d1380595b5463
UnicornDad8 <unicorndad8@gmail.com>
2025-02-06 19:27:06 -0300

    Test filtering on BrowseProductsPage component

--- tests/pages/BrowseProductsPage.test.tsx
+++ tests/pages/BrowseProductsPage.test.tsx
@@ -123,4 +123,51 @@ describe("BrowseProductsPage", () => {
       expect(screen.getByText(product.name)).toBeInTheDocument()
     })
   });
+
+  it("should filter products by category", async () => {
+    const { getCategoriesSkeleton, getCategoriesComboBox } = renderComponent();
+
+    await waitForElementToBeRemoved(getCategoriesSkeleton);
+    const combobox = getCategoriesComboBox();
+    const user = userEvent.setup();
+    await user.click(combobox!);
+
+    const selectedCategory = categories[0];
+    const option = screen.getByRole("option", { name: selectedCategory.name });
+    await user.click(option);
+
+    const products = db.product.findMany({
+      where: {
+        categoryId: { equals: selectedCategory.id }
+      }
+    });
+    const rows = screen.getAllByRole("row");
+    const dataRows = rows.slice(1);
+    expect(dataRows).toHaveLength(products.length);
+
+    products.forEach(product => {
+      expect(screen.getByText(product.name)).toBeInTheDocument();
+    })
+  });
+
+  it("should render all products if All category is selected", async () => {
+    const { getCategoriesSkeleton, getCategoriesComboBox } = renderComponent();
+
+    await waitForElementToBeRemoved(getCategoriesSkeleton);
+    const combobox = getCategoriesComboBox();
+    const user = userEvent.setup();
+    await user.click(combobox!);
+
+    const option = screen.getByRole("option", { name: /all/i });
+    await user.click(option);
+
+    const products = db.product.getAll();
+    const rows = screen.getAllByRole("row");
+    const dataRows = rows.slice(1);
+    expect(dataRows).toHaveLength(products.length);
+
+    products.forEach(product => {
+      expect(screen.getByText(product.name)).toBeInTheDocument();
+    })
+  });
 });
