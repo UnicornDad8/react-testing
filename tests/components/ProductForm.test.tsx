79df746c968df0758681a4ff0af7d53d93ee881d
UnicornDad8 <unicorndad8@gmail.com>
2025-02-14 11:51:34 -0300

    Test price validation rules on ProductForm component

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -95,4 +95,47 @@ describe("ProductForm", () => {
     expect(error).toBeInTheDocument();
     expect(error).toHaveTextContent(errorMessage);
   });
+
+  it.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "0",
+      price: 0,
+      errorMessage: /1/,
+    },
+    {
+      scenario: "negative",
+      price: -1,
+      errorMessage: /1/,
+    },
+    {
+      scenario: "greater than 1000",
+      price: 1001,
+      errorMessage: /1000/,
+    },
+    {
+      scenario: "not a number",
+      price: "a",
+      errorMessage: /required/,
+    },
+  ])("should display an error if price is $scenario", async ({ price, errorMessage }) => {
+    const { waitForFormToLoad } = renderComponent();
+
+    const { nameInput, priceInput, categoryInput, submitButton } = await waitForFormToLoad();
+    const user = userEvent.setup();
+    await user.type(nameInput, "a");
+    if(price !== undefined)
+      await user.type(priceInput, price.toString());
+    await user.click(categoryInput);
+    const options = screen.getAllByRole("option");
+    await user.click(options[0]);
+    await user.click(submitButton);
+
+    const error = screen.getByRole("alert");
+    expect(error).toBeInTheDocument();
+    expect(error).toHaveTextContent(errorMessage);
+  });
 });
